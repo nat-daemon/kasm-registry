b822fbea3ad72a7354dcf4cc379709b1720f426f
bastionbot <droneship@gmail.com>
2025-04-22 21:50:38 -0400

    updated next.config.js...

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    name: 'NAT Daemon\'s toys',
+    description: 'Red team stuff',
+    icon: 'https://nat-daemon.github.io/kasm-registry/1.1/logo.png',
+    listUrl: 'https://nat-daemon.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/nat-daemon/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
